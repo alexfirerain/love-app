1b7323a8d73b0ef68e2bc59784b198e1fe875786
Alexander Lukyanov <lux-nova@mail.ru>
2022-07-23 19:59:42 +0300

    init

---
+++ src/test/java/org/swetophor/loveapp/LoveAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.swetophor.loveapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
